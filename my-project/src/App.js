c0114c8fd772e9851eaddbe4909b485e73735167
00lar <eduardani84@gmail.com>
2023-10-06 00:53:47 -0600

    Hook implemented, and counter A and B

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,34 +1,48 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
 
-function App() {
+const useCounter = () => {
   const [Counter,setCounter] = useState(0);
-
-  const Add = () => {
-    setCounter(Counter+1);
+  const Add = () => {setCounter(Counter+1);} 
+  const Substract = () => {setCounter(Counter-1);}
+  const Reset = () => {setCounter(0)};
+  return {
+    Counter,
+    Add,
+    Substract,
+    Reset
   }
+}
 
-  const Substract = () => {
-    setCounter(Counter-1);
-  }
-
-  const Reset = () => {
-    setCounter(0);
-  }
-
+function App() {
+  const CounterA = useCounter();
+  const CounterB = useCounter()
   return (
     <div className="App">
-    <p>{Counter}</p>
-      <button
-      onClick={Add}>+</button>
-      <button
-      onClick={Substract}
-      >-</button>
-      <button
-      onClick={Reset}>Reset</button>
+      <div>
+        <p>{CounterA.Counter}</p>
+        <button
+        onClick={CounterA.Add}>+</button>
+        <button
+        onClick={CounterA.Substract}
+        >-</button>
+        <button
+        onClick={CounterA.Reset}>Reset</button>
+      </div>
+      <div>
+        <p>{CounterB.Counter}</p>
+        <button
+        onClick={CounterB.Add}>+</button>
+        <button
+        onClick={CounterB.Substract}
+        >-</button>
+        <button
+        onClick={CounterB.Reset}>Reset</button>
+      </div>
+    
     </div>
+
   );
 }
 
